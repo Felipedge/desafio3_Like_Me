897f4afd6534a12fa93e9f38c2be1afeae92e539
Felipe P <fpalmasaldivia@gmail.com>
2023-01-08 21:38:39 -0300

    actualizacion desafio

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const { obtenerPost, agregarPost } = require('./info.js');
 app.use(express.json());
 app.use(cors());
 
-
-
-
-app.use(express.static("public"));
-
 app.use(express.json())
 app.use(express.static("public"));
 
@@ -42,6 +37,33 @@ app.post("/posts", async (req, res) => {
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
+})
+
+app.put("/posts/:id", async (req, res) => {
+    const { id } = req.params
+    const { likes } = req.query
+    try {
+        await updateLikes(likes, id)
+        res.send("Likes modificado")
+    } catch ({ code, message }) {
+        res.status(code).send(message)
+    }
+})
+
+app.put("/posts/like/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        await incrementLikes(id)
+        res.send("Likes aumentados")
+    } catch ({ code, message }) {
+        res.status(code).send(message)
+    }
+})
+
+app.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params
+    await deletePost(id)
+    res.send("Post eliminado")
 })
 
 app.listen(3000, console.log("servidor arriba"));
--- info.js
+++ info.js
@@ -16,10 +16,36 @@ const agregarPost = async (titulo, img, descripcion) => {
 
 };
 
-const obtenerPost = async () =>{
-    const {rows} = await pool.query("SELECT * FROM posts");
+const obtenerPost = async () => {
+    const { rows } = await pool.query("SELECT * FROM posts");
     console.log(rows)
     return rows
 }
+
+const updateLikes = async (likes, id) => {
+    const consulta = "UPDATE posts SET likes = $1 WHERE id = $2"
+    const values = [likes, id]
+    const { rowCount } = await pool.query(consulta, values)
+    if (rowCount === 0) {
+        throw { code: 404, message: "No se pudo realizar la acccion" }
+    }
+}
+
+const incrementLikes = async (id) => {
+    const consulta = "UPDATE posts SET likes = likes + 1 WHERE id = $1"
+    const values = [id]
+    const { rowCount } = await pool.query(consulta, values)
+    if (rowCount === 0) {
+        throw { code: 404, message: "No se pudo realizar la accion" }
+    }
+}
+
+
+const deletePost = async (id) => {
+    const consulta = "DELETE FROM posts WHERE id = $1"
+    const values = [id]
+    const result = await pool.query(consulta, values)
+}
+
 
 module.exports = { agregarPost, obtenerPost };
